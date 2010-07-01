3ced0a5f4b2b07cc92cb4c10c6f2f124b58b0136
Luis Pedro Coelho <lpc@cmu.edu>
2010-07-01 12:31:38 +0200

    Basic infrastructure

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/zsh
+function build() {
+    input=$1
+    mkdir -p .$input.tex_files
+    cd .$input.tex_files
+    TEXINPUTS=..:.:../images/:../figures/: pdflatex $input
+    BSTINPUTS=:..: BIBINPUTS=:..:.: bibtex $input
+    cp $input.pdf ..
+    cd ..
+}
+build report 
+
